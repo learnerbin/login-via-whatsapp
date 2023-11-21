1f90c55e48e1169ab79f90e058e2b8a56833bd62
Shabin-k <helloshabink@gmail.com>
2023-11-21 19:08:42 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+  res.render("index");
 });
 
+router.post("/generate", (req, res) => {
+  const { phone } = req.body;
+  res.send(`Signup successful!  Phone: ${phone}`);
+});
+
+router.post("/verify", (req, res) => {
+  const { otp } = req.body;
+  const storedOTP = "1234"; // Replace with your stored OTP
+
+  if (otp === storedOTP) {
+    res.render("index", { success: true });
+  } else {
+    res.render("index", { success: false });
+  }
+});
 module.exports = router;
